de8dcca438ce11c44b6ce7e68591906296ae2e8c
jesusCode-code <jesusmasna45@gmail.com>
2025-11-26 15:23:08 -0500

    public_footer.php

--- includes/public_footer.php
+++ includes/public_footer.php
@@ -112,6 +112,10 @@
                             <i class="bi bi-person-circle"></i>
                             <span>Rocio Vargas llantoy</span>
                         </div>
+                        <div class="team-member">
+                            <i class="bi bi-person-circle"></i>
+                            <span>Ing. Luis Alfredo Castillon Siguas</span>
+                        </div>
                     </div>
                     
                     <!-- Newsletter opcional -->
@@ -172,4 +176,5 @@ scrollToTopBtn.addEventListener('click', function() {
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
+
 </html>
