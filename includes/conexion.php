623c7726a37177ad9e248e8be96853a99385b8e3
jesusCode-code <jesusmasna45@gmail.com>
2025-11-25 22:56:29 -0500

    conexion.php

--- includes/conexion.php
+++ includes/conexion.php
@@ -1,20 +1,20 @@
 <?php
+// Configuración de la conexión a SQL Server (Azure)
+$serverName = "adminchocolates.database.windows.net"; 
 
-// Configuración de la conexión a SQL Server
-$serverName = "DESKTOP-MC1FKRQ"; // ej: "localhost" o "MI-PC\SQLEXPRESS"
 $connectionOptions = array(
-    // ¡Actualizado con el nombre de tu BD!
     "Database" => "SIS_VENTAS_CHOCOLATES_HELENA_V1", 
-    "Uid" => "sa",       
-    "PWD" => "123"
+    "Uid" => "chocolates",       
+    "PWD" => "a1234567_",
+    // Es recomendable añadir esto para evitar problemas con tildes y caracteres especiales
+    "CharacterSet" => "UTF-8" 
 );
 
-// Conectar
+// Intentar conectar
 $conn = sqlsrv_connect($serverName, $connectionOptions); 
 
 if ($conn === false) {
-    // Si la conexión falla, muestra el error y detiene la ejecución
+    // Si la conexión falla, muestra el error detallado y detiene la ejecución
     die(print_r(sqlsrv_errors(), true)); 
 }
-
 ?>
